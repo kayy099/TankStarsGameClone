0b52fce672ddcba7c9a822ce853598a53c6f98be
kayy099 <108053412+kayy099@users.noreply.github.com>
2022-12-21 23:07:24 +0530

    Add files via upload

---
+++ core/src/com/badlogic/drop/Tank1.java
@@ -0,0 +1,14 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Tank1 extends Tank{
+
+    public Tank1(int health, int power) {
+        super(health, power);
+    }
+
+    public Tank1(Texture tank1, Texture tank2) {
+        super(tank1, tank2);
+    }
+}
---
+++ core/src/com/badlogic/drop/Tank2.java
@@ -0,0 +1,14 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Tank2 extends Tank{
+
+    public Tank2(int health, int power) {
+        super(health, power);
+    }
+
+    public Tank2(Texture tank1, Texture tank2) {
+        super(tank1, tank2);
+    }
+}
